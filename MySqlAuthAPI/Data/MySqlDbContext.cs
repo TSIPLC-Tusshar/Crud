6216a578d9f3a5d8d8b5ce674d08e8cc237fa361
TSIPLC-Tusshar <tusshar.k@tsiplc.com>
2025-06-03 17:14:17 +0530

    modified changes

--- MySqlAuthAPI/Data/MySqlDbContext.cs
+++ MySqlAuthAPI/Data/MySqlDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 using MySqlAuthAPI.Data.Entities;
@@ -12,64 +13,15 @@ namespace MySqlAuthAPI.Data
 
         }
 
-        public virtual DbSet<Aspnetrole> Aspnetroles { get; set; }
-
-        public virtual DbSet<Aspnetroleclaim> Aspnetroleclaims { get; set; }
-
         public virtual DbSet<Aspnetuser> Aspnetusers { get; set; }
-
-        public virtual DbSet<Aspnetuserclaim> Aspnetuserclaims { get; set; }
-
-        public virtual DbSet<Aspnetuserlogin> Aspnetuserlogins { get; set; }
-
-        public virtual DbSet<Aspnetusertoken> Aspnetusertokens { get; set; }
-
-        public virtual DbSet<Efmigrationshistory> Efmigrationshistories { get; set; }
 
         public virtual DbSet<UserMaster> UserMasters { get; set; }
 
         public virtual DbSet<LoginSession> LoginSessions { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //    => optionsBuilder.UseMySQL(_configuration.GetConnectionString("Database"));
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Aspnetrole>(entity =>
-            {
-                entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-                entity.ToTable("aspnetroles");
-
-                entity.HasIndex(e => e.NormalizedName, "RoleNameIndex").IsUnique();
-
-                entity.Property(e => e.ConcurrencyStamp).HasDefaultValueSql("'NULL'");
-                entity.Property(e => e.Name)
-                    .HasMaxLength(256)
-                    .HasDefaultValueSql("'NULL'");
-                entity.Property(e => e.NormalizedName)
-                    .HasMaxLength(256)
-                    .HasDefaultValueSql("'NULL'");
-            });
-
-            modelBuilder.Entity<Aspnetroleclaim>(entity =>
-            {
-                entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-                entity.ToTable("aspnetroleclaims");
-
-                entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
-
-                entity.Property(e => e.Id).HasColumnType("int(11)");
-                entity.Property(e => e.ClaimType).HasDefaultValueSql("'NULL'");
-                entity.Property(e => e.ClaimValue).HasDefaultValueSql("'NULL'");
-
-                entity.HasOne(d => d.Role).WithMany(p => p.Aspnetroleclaims)
-                    .HasForeignKey(d => d.RoleId)
-                    .HasConstraintName("FK_AspNetRoleClaims_AspNetRoles_RoleId");
-            });
 
             modelBuilder.Entity<Aspnetuser>(entity =>
             {
@@ -101,91 +53,40 @@ namespace MySqlAuthAPI.Data
                 entity.Property(e => e.UserName)
                     .HasMaxLength(256)
                     .HasDefaultValueSql("'NULL'");
-
-                entity.HasMany(d => d.Roles).WithMany(p => p.Users)
-                    .UsingEntity<Dictionary<string, object>>(
-                        "Aspnetuserrole",
-                        r => r.HasOne<Aspnetrole>().WithMany()
-                            .HasForeignKey("RoleId")
-                            .HasConstraintName("FK_AspNetUserRoles_AspNetRoles_RoleId"),
-                        l => l.HasOne<Aspnetuser>().WithMany()
-                            .HasForeignKey("UserId")
-                            .HasConstraintName("FK_AspNetUserRoles_AspNetUsers_UserId"),
-                        j =>
-                        {
-                            j.HasKey("UserId", "RoleId").HasName("PRIMARY");
-                            j.ToTable("aspnetuserroles");
-                            j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
-                        });
             });
 
-            modelBuilder.Entity<Aspnetuserclaim>(entity =>
+            modelBuilder.Entity<IdentityRole>(entity =>
             {
-                entity.HasKey(e => e.Id).HasName("PRIMARY");
+                entity.ToTable("aspnetroles");
+            });
 
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("aspnetuserroles");
+            });
+
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+            {
                 entity.ToTable("aspnetuserclaims");
-
-                entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
-
-                entity.Property(e => e.Id).HasColumnType("int(11)");
-                entity.Property(e => e.ClaimType).HasDefaultValueSql("'NULL'");
-                entity.Property(e => e.ClaimValue).HasDefaultValueSql("'NULL'");
-
-                entity.HasOne(d => d.User).WithMany(p => p.Aspnetuserclaims)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserClaims_AspNetUsers_UserId");
             });
 
-            modelBuilder.Entity<Aspnetuserlogin>(entity =>
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
             {
-                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey }).HasName("PRIMARY");
-
-                entity.ToTable("aspnetuserlogins");
-
-                entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
-
-                entity.Property(e => e.ProviderDisplayName).HasDefaultValueSql("'NULL'");
-
-                entity.HasOne(d => d.User).WithMany(p => p.Aspnetuserlogins)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserLogins_AspNetUsers_UserId");
-            });
-
-            modelBuilder.Entity<Aspnetusertoken>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name }).HasName("PRIMARY");
-
-                entity.ToTable("aspnetusertokens");
-
-                entity.Property(e => e.Value).HasDefaultValueSql("'NULL'");
-
-                entity.HasOne(d => d.User).WithMany(p => p.Aspnetusertokens)
-                    .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK_AspNetUserTokens_AspNetUsers_UserId");
-            });
-
-            modelBuilder.Entity<Efmigrationshistory>(entity =>
-            {
-                entity.HasKey(e => e.MigrationId).HasName("PRIMARY");
-
-                entity.ToTable("__efmigrationshistory");
-
-                entity.Property(e => e.MigrationId).HasMaxLength(150);
-                entity.Property(e => e.ProductVersion).HasMaxLength(32);
+                entity.ToTable("aspnetroleclaims");
             });
 
             modelBuilder.Entity<LoginSession>(entity =>
             {
                 entity.HasKey(e => e.SessionId).HasName("PRIMARY");
 
-                entity.ToTable("LoginSessions");
+                entity.ToTable("loginsessions");
             });
 
             modelBuilder.Entity<UserMaster>(entity =>
             {
                 entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-                entity.ToTable("UserMaster");
+                entity.ToTable("usermaster");
             });
 
             OnModelCreatingPartial(modelBuilder);
